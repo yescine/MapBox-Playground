15034d7be343b74c55f4c4121b0575c6bc693855
yassine <63535911+yescine@users.noreply.github.com>
2020-12-25 20:00:51 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./styles.css";
+import { Paper } from "@material-ui/core";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import TabContext from "@material-ui/lab/TabContext";
+import TabList from "@material-ui/lab/TabList";
+import { TabPanel } from "@material-ui/lab";
+
+// Material Icons
+import AddToQueueIcon from "@material-ui/icons/AddToQueue";
+import AirportShuttleIcon from "@material-ui/icons/AirportShuttle";
+import ApartmentIcon from "@material-ui/icons/Apartment";
+// Import View Components
+import MapView from "./views/MapBox";
+import Container2 from "./container/Container2";
+
+export default function App() {
+  const [tab, setTab] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <Paper square>
+        <TabContext value={tab}>
+          <TabList
+            onChange={(event, NewValue) => setTab(NewValue)}
+            variant="standard"
+            indicatorColor="primary"
+            textColor="primary"
+          >
+            <Tab icon={<AddToQueueIcon />} label="view 1" value={0} />
+            <Tab icon={<AirportShuttleIcon />} label="container 2" value={1} />
+            <Tab icon={<ApartmentIcon />} label="component 3" value={2} />
+          </TabList>
+          <TabPanel style={{ padding: 0, paddingTop: "0.5rem" }} value={0}>
+            <MapView />
+          </TabPanel>
+          <TabPanel value={1}>
+            <Container2 />
+          </TabPanel>
+          <TabPanel value={2}>number 4</TabPanel>
+        </TabContext>
+      </Paper>
+    </div>
+  );
+}
---
+++ src/container/Container2.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+export default function Container2(props) {
+  return (
+    <>
+      <div>Container2</div>
+    </>
+  );
+}
